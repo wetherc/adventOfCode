20bf024c239f47211de3f0080f8c15507f4d398b
faulconbridge <chris@chriswetherill.me>
2017-12-12 19:45:08 -0500

    Day 03 part 02 WIP

--- src/03.py
+++ src/03.py
@@ -24,6 +24,31 @@ def calculate_spiral(n):
     else:
         return (k, k - (m - n - t))
 
+def get_neighbors(coords):
+    x = coords[0]
+    y = coords[1]
+    neighbors = [
+        (x - 1, y - 1),  # lower left
+        (x, y -  1),  # lower center
+        (x, y + 1),  # lower right
+        (x + 1, y),  # center right
+        (x + 1, y + 1),  # upper right
+        (x, y + 1),  # upper center
+        (x - 1, y + 1),  # upper left
+        (x, y - 1)  # center left
+    ]
+    return neighbors
+
+
+def spiralize(n):
+    points, value = [[1]], 1
+    coords = (0, 0)
+
+    while value < n:
+        value = sum(map(lambda x: points[x[0]][x[1]], get_neighbors(coords)))
+        points[coords[0]][coords[1]].append(value)
+    return value
+
 
 if __name__ == '__main__':
     coords = calculate_spiral(int(args.input))
