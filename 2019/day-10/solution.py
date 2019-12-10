ecb40bb185887adf684b0ac4257b5f87adc8a615
Christopher Wetherill <chris@tbmh.org>
2019-12-10 12:49:01 -0500

    Working solution to part 1, now with double the shame

--- 2019/day-10/solution.py
+++ 2019/day-10/solution.py
@@ -19,7 +19,7 @@ def load_input() -> List[Point]:
     '''
     points = []
     row = 0
-    with open('./input_test.txt', 'r') as f:
+    with open('./input.txt', 'r') as f:
         for line in f:
             coords = [elem for elem in line]
             for idx, elem in enumerate(coords):
@@ -29,7 +29,7 @@ def load_input() -> List[Point]:
     return points
 
 
-def check_is_blocking(start: Point, end: Point, midpoints: List[Point]) -> bool:
+def check_is_blocked(start: Point, end: Point, midpoints: List[Point]) -> bool:
     '''Uses the crossproduct of (b-a) and (c-a) to determine
     if the points are aligned (a crossproduct of 0 denotes that
     we form a 0-area triangle among the three points, implying
@@ -48,6 +48,8 @@ def check_is_blocking(start: Point, end: Point, midpoints: List[Point]) -> bool:
             (end.y - start.y)
         )
 
+        # skip to the next candidate point if
+        # area of the triangle is non-zero
         if abs(cross_product) != 0:
             continue
 
@@ -57,6 +59,10 @@ def check_is_blocking(start: Point, end: Point, midpoints: List[Point]) -> bool:
             (mid.y - start.y) *
             (end.y - start.y)
         )
+
+        # skip to the next candidate point if
+        # midpoint is outside the bounts of the
+        # start/endpoints
         if dot_product < 0:
             continue
 
@@ -66,10 +72,11 @@ def check_is_blocking(start: Point, end: Point, midpoints: List[Point]) -> bool:
             (end.y - start.y) *
             (end.y - start.y)
         )
-        if dot_product > squared_length_ba:
-            return False
 
-    return True
+        if dot_product > squared_length_ba:
+            return True
+
+    return False
 
 
 def main():
@@ -78,6 +85,13 @@ def main():
     # and that makes me hate myself, but I'm also too lazy
     # to do anything about it until it becomes an actual
     # problem, so that's where I'm at right now.
+    max_point = Point(0, 0)
+    max_nodes_visible = 0
+
+    print(f'Iterating through all candidate asteroids')
+    print(
+        f'This is in O(n^3) runtime complexity, so grab '
+        f'some coffee or something while you wait...')
     for asteroid in asteroids:
         endpoints = [
             endpoint for endpoint in asteroids
@@ -85,16 +99,17 @@ def main():
         for endpoint in endpoints:
             midpoints = [
                 midpoint for midpoint in endpoints
-                if (abs(endpoint.x), abs(endpoint.y)) <= \
-                   (abs(midpoint.x), abs(midpoint.y)) <= \
-                   (abs(asteroid.x), abs(asteroid.y))
-            ]
+                if (midpoint.x, midpoint.y) != (endpoint.x, endpoint.y)]
             _visible_to_src = True
-            if not midpoints:
-                continue
-            if not check_is_blocking(asteroid, endpoint, midpoints):
+            if not check_is_blocked(asteroid, endpoint, midpoints):
                 asteroid.num_visible += 1
-        print(f'{(asteroid.x, asteroid.y)}: {asteroid.num_visible}')
+        if asteroid.num_visible > max_nodes_visible:
+            max_nodes_visible = asteroid.num_visible
+            max_point = asteroid
+
+    print(
+        f'Asteroid at point {(max_point.x, max_point.y)} '
+        f'has {max_nodes_visible} visible')
 
 
 if __name__ == '__main__':
