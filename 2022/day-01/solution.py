7406b119f61bc64f29ff95212beaf8b1a84b4b81
Christopher Wetherill <1910269+wetherc@users.noreply.github.com>
2022-12-01 14:34:41 +0000

    2022 day 1 solutions

---
+++ 2022/day-01/solution.py
@@ -0,0 +1,31 @@
+def parse_input() -> list[list[int]]:
+    parsed_input: list[list[int]] = []
+    with open('/workspaces/adventOfCode/2022/day-01/input.txt', 'r', encoding='utf-8') as f:
+        _tmp: list[int] = []
+        for line in f:
+            _line = line.strip()
+            if _line:
+                _tmp.append(int(_line))
+            else:
+                parsed_input.append(_tmp)
+                _tmp = []
+    return parsed_input
+
+
+def collect_calories(elves: list[list[int]]) -> list[int]:
+    elf_calories: list[int] = []
+    for elf in elves:
+        elf_calories.append(sum(elf))
+    return elf_calories
+
+
+def main():
+    elf_loads = parse_input()
+    elf_calories = sorted(collect_calories(elf_loads), reverse=True)
+
+    print(f'The elf carrying the most has {elf_calories[0]} calories')
+    print(f'The top 3 elves together are carrying {sum(elf_calories[0:3])}')
+    
+
+if __name__ == '__main__':
+    main()
