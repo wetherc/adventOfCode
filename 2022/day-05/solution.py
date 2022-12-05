6ae1f14723554212952cf1f8142fa17006699c70
Christopher Wetherill <1910269+wetherc@users.noreply.github.com>
2022-12-05 13:48:28 +0000

    Add solution for Day 05
    this is working but broken. I'm doing something dumb that's causing
    the output from the first part of the puzzle to be re-used as the
    input to the second part of the puzzle. Likely because I'm not making
    a deep copy of the input list of crates. I'll fix in the next commit
    but making coffee first...

---
+++ 2022/day-05/solution.py
@@ -0,0 +1,69 @@
+import os
+
+
+def parse_input():
+    parsed_input = {
+        'stacks': [],
+        'instructions': []
+    }
+    with open(
+        os.path.dirname(os.path.abspath(__file__)) + '/input.txt',
+        'r',
+        encoding='utf-8') as f:
+        for line in f:
+            if ord(line[0]) <= 90:
+                parsed_input['stacks'].append(
+                    line.strip().split()
+                )
+            else:
+                parsed_input['instructions'].append(
+                    list(
+                        int(line.strip().split()[index])
+                        for index in [1, 3, 5]
+                    )
+                )
+    return parsed_input
+
+
+def rearrange_crates(crates: list[list[str]], num: int, from_pos: int,
+                     to_pos: int, preserve_order=False):
+    _crates = crates[from_pos - 1][(-1 * num):]
+    del crates[from_pos - 1][(-1 * num):]
+    
+    if preserve_order:
+        for crate in _crates:
+            crates[to_pos - 1].append(crate)
+    else:
+        for crate in _crates[::-1]:
+            crates[to_pos - 1].append(crate)
+    return crates
+
+
+def main():
+    parsed_input = parse_input()
+    rearranged_crates = parsed_input['stacks']
+    for instruction in parsed_input['instructions']:
+        rearranged_crates = rearrange_crates(
+            rearranged_crates,
+            instruction[0],
+            instruction[1],
+            instruction[2]
+        )
+    
+    print(''.join([crate[len(crate) - 1] for crate in rearranged_crates]))
+
+    rearranged_crates_ordered = parsed_input['stacks']
+    for instruction in parsed_input['instructions']:
+        rearranged_crates_ordered = rearrange_crates(
+            rearranged_crates_ordered,
+            instruction[0],
+            instruction[1],
+            instruction[2],
+            True
+        )
+    print(rearranged_crates_ordered)
+    print(''.join([crate[len(crate) - 1] for crate in rearranged_crates_ordered]))
+
+
+if __name__ == '__main__':
+    main()
