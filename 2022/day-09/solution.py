6408ed285c6ef7d2afb5b436be09c5a363ac654a
Christopher Wetherill <1910269+wetherc@users.noreply.github.com>
2022-12-15 01:11:06 +0000

    day 09 part 2 corrections

--- 2022/day-09/solution.py
+++ 2022/day-09/solution.py
@@ -14,8 +14,8 @@ def parse_input() -> list[list[str]]:
 
 
 def cmp(a: int, b: int) -> int:
-    return (a > b) - (a < b) 
-    
+    return (a > b) - (a < b)
+
 
 def move_rope(rope: list[list[int]], direction: str, magnitude: int):
     _directions = {
@@ -28,7 +28,7 @@ def move_rope(rope: list[list[int]], direction: str, magnitude: int):
     tail_positions = []
     for _ in range(magnitude):
         rope[0] = list(map(add, rope[0], _directions[direction]))
-        
+
         for knot in range(1, len(rope)):
             rope[knot] = follow_leader(rope[knot], rope[knot - 1])
         tail_positions.append(tuple(rope[-1]))
@@ -39,11 +39,21 @@ def follow_leader(follower: list[int],
                   leader: list[int]) -> list[int]:
     _distance = list(map(sub, leader, follower))
 
+    # Movement of a trailing knot only happens to prevent
+    # a discontinuity --- any case where there would be
+    # an empty space between the leading and following
+    # knots (distance = 2)
     if any(abs(elem) > 1 for elem in _distance):
-        _idx = _distance.index(max(_distance, key=abs))
-        _sign = cmp(_distance[_idx], 0)
-
-        _distance[_idx] =  _distance[_idx] + (_sign * -1)
+        # we can get slightly larger diagonal jumps
+        # in longer chains that complicates things a
+        # little bit
+        _signs = [cmp(val, 0) for val in _distance]
+        _distance = [
+            val + (_signs[idx] * -1)
+            if abs(val) > 1 else val
+            for idx, val in enumerate(_distance)
+        ]
+        
         follower = list(map(add, follower, _distance))
     return follower
 
@@ -52,7 +62,7 @@ def simulate(num_knots, parsed_input):
     # Simulation with a single tail knot
     tail_history = [(0, 0)]
     # Elem 0 is the head, elem 1 is the first knot, and so on
-    knot_positions = [[0, 0]] * (1 + num_knots)
+    knot_positions = [[0, 0]  for _ in range(1 + num_knots)]
 
     for instruction in parsed_input:
         knot_positions, _tail_movement = move_rope(
