b0adaff24d3daaecf2848bcad4aecb86f7ebbb46
Christopher Wetherill <git@tbmh.org>
2023-12-02 11:25:43 -0500

    Day 02 solution

---
+++ 2023/day_02/solution.py
@@ -0,0 +1,64 @@
+import re
+
+
+def load_input():
+    input = []
+    with open('input.txt', 'r') as f:
+        for line in f:
+            _parts = re.split(': |; ', line.strip())
+            _item = {
+                'game': _parts[0].split()[-1],
+                'rounds': [
+                    dict(reversed(elem.split()) for elem in part.split(', '))
+                    for part in _parts[1:]
+                ]
+            }
+            input.append(_item)
+
+    return input
+
+
+def cube_totals(game, max_red, max_green, max_blue):
+    if (max([int(_round.get('red', 0)) for _round in game]) > max_red):
+        return False
+    if (max([int(_round.get('green', 0)) for _round in game]) > max_green):
+        return False
+    if (max([int(_round.get('blue', 0)) for _round in game]) > max_blue):
+        return False
+    
+    return True
+
+
+def fewest_dice(game):
+    _max_red = 0
+    _max_green = 0
+    _max_blue = 0
+
+    for _round in game['rounds']:
+        _max_red = max(
+            _max_red,
+            int(_round.get('red', 0)))
+        _max_green = max(
+            _max_green,
+            int(_round.get('green', 0)))
+        _max_blue = max(
+            _max_blue,
+            int(_round.get('blue', 0)))
+    
+    return _max_red * _max_green * _max_blue
+
+
+if __name__ == '__main__':
+    parsed_rounds = load_input()
+
+    valid_games = [
+        int(game['game']) for game in parsed_rounds
+        if cube_totals(
+            game['rounds'],
+            12,
+            13,
+            14)
+    ]
+
+    print(sum(valid_games))
+    print(sum([fewest_dice(game) for game in parsed_rounds]))
