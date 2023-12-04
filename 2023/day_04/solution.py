4d2905bfc100d4c8b952ca4f507aaf4fc9f6d8ca
Christopher Wetherill <git@tbmh.org>
2023-12-04 09:58:36 -0500

    clean up day 04 a bit

--- 2023/day_04/solution.py
+++ 2023/day_04/solution.py
@@ -8,13 +8,20 @@ def load_input():
             _parts = re.split(': |\| ', line.strip())
             _item = {
                 'card': int(_parts[0].split()[-1]),
-                'winning': [int(elem) for elem in re.split(' +', _parts[1]) if elem],
-                'picks': [int(elem) for elem in re.split(' +', _parts[2]) if elem],
+                'winning': [
+                    int(elem)
+                    for elem in re.split(' +', _parts[1])
+                    if elem],
+                'picks': [
+                    int(elem)
+                    for elem in re.split(' +', _parts[2])
+                    if elem],
                 'card_count': 1
             }
             input.append(_item)
 
     return input
+
 
 def calculate_points(card):
     doubling_len = len(set(card['winning']).intersection(set(card['picks'])))
@@ -22,7 +29,7 @@ def calculate_points(card):
         return 0, 0
     
     points = 1
-    for _step in range(1, doubling_len):
+    for _ in range(1, doubling_len):
         points *= 2
     
     return points, doubling_len
@@ -31,11 +38,18 @@ def calculate_points(card):
 if __name__ == '__main__':
     input = load_input()
     for card in input:
+        # Card numbers are 1-indexed so for list indices we need to decrement
         input[card['card'] - 1]['points'], _extras = calculate_points(card)
         for extra_card in range(card['card'], card['card'] + _extras):
+            # for each winning number match, we add a new copy of a
+            # scratchoff ticket from n + 1 through n + (1 * num_matches)
             input[extra_card]['card_count'] += 1 * card['card_count']
     
     print(f'Your original scratchcard count was {len(input)}')
-    print(f'Your scratchcards totalled {sum([card["points"] for card in input])} points')
-    print(f'Your scratchcard count totalled {sum([card["card_count"] for card in input])} cards')
+    print(
+        'Your scratchcards totalled',
+        f'{sum([card["points"] for card in input])} points')
+    print(
+        'Your scratchcard count totalled',
+        f'{sum([card["card_count"] for card in input])} cards')
 
